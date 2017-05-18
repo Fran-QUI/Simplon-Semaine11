6ce19cb0415468ee5c73544ada3511c78bf7a113
François QUIVRON <fquivron@gmail.com>
2017-05-18 13:04:21 +0200

    crée les fichiers et l'arborescence, début du projet plusoumoins

---
+++ js/plusoumoins.js
@@ -0,0 +1,37 @@
+var $text = document.getElementById("texteInformation");
+var nombreIa;
+var nombreJoueur;
+var $valider = document.getElementById("valider");
+var resultat = "entrez un nombre entre 1 et 100";
+
+//définir un nombre aléatoire
+function definirNombreIa() {
+	nombreIa=Math.floor(Math.random() * 100);
+}
+
+//défnir le nombre joueur
+function definirNombreJoueur() {
+	nombreJoueur= document.getElementById("nombreJoueur");
+}
+
+//comparer le nombre avec le nombre aléatoire
+function comparer() {
+	if (nombreIa==nombreJoueur) {
+		resultat = "trouvé";
+	} else if (nombreIa<nombreJoueur) {
+		resultat = "trop grand";
+	} else {
+		resultat = "trop petit";
+	}
+	afficher()
+}
+
+//afficher le résultat
+function afficher() {
+	$text.innerHTML() = resultat;
+}
+
+$valider.onclick() = comparer()
+
+definirNombreIa();
+afficher();
